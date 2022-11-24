566c4182e28293709e0b541b3d753d8a60ed3e72
Rafael Ledesma Soto <rlsdev13@gmail.com>
2022-11-23 23:00:13 -0600

    main ADD project structure and login component

---
+++ src/components/WelcomeItem.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>welcome item</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+  ],
+}
