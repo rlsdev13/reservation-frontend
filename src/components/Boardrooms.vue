a22ad82713fb9275c02bf2567b80fe75ede9c66e
Rafael Ledesma Soto <rlsdev13@gmail.com>
2022-11-24 03:53:00 -0600

    develop ADD components to show boardrooms

---
+++ src/components/Boardrooms.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Card from './Card.vue';
+
+</script>
+
+<template>
+    <div class="flex flex-wrap px-2 mt-4">
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-4">
+            <!-- <Card /> -->
+        </div>
+        
+    </div>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    defineProps<{
+        name : string;
+        imageUrl : string;
+        description : string;
+    }>()
+</script>
+
+<template>
+    <div class="card card-side bg-base-100 shadow-xl">
+        <figure><img src="https://placeimg.com/200/280/arch" alt="Movie" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Watch</button>
+                <button class="btn btn-primary">Watch</button>
+            </div>
+        </div>
+    </div>
+</template>
